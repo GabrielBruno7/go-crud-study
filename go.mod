2fe5d0e7854e4dbabe41c29d874acf250459c855
gabrielbruno7 <gabriel.bruno1w@gmail.com>
2024-05-08 10:17:33 -0300

    Organizar pastas

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/GabrielBruno7/go-crud-study
+
+go 1.19
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Deu erro fi na env")
+	}
+	fmt.Println(os.Getenv("Password"))
+}
