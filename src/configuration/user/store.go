95bc87e797dd6cb3a912e21c7c6dd3cfc7c25924
gabrielbruno7 <gabriel.bruno1w@gmail.com>
2024-05-11 08:56:44 -0300

    Feat: Create a request and response to user json

--- src/configuration/user/store.go
+++ src/configuration/user/store.go
@@ -1,12 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/GabrielBruno7/go-crud-study/src/configuration/rest_err"
+	"github.com/GabrielBruno7/go-crud-study/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+	
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
 
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+			c.JSON(restErr.Code, restErr)
+
+			return
+	}
+
+	fmt.Println(userRequest);
 }
